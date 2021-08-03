0133f7285a9edac1c6ffde1342657ca81618f3de
IT21147814 <87793117+IT21147814@users.noreply.github.com>
2021-08-03 13:42:38 +0000

    create structure called box, assigning variables, correct the assigning values, implement the function

--- exercise02.cpp
+++ exercise02.cpp
@@ -7,6 +7,7 @@ int main() {
     int box1Height, box1Width, box1Length;
     int box2Height, box2Width, box2Length;
     int totalVolume, totalSurface;
+ 
     
     cout << "Enter Box 1 Height : ";
     cin >> box1Height;
@@ -15,15 +16,14 @@ int main() {
     cout << "Enter Box 1 Length : ";
     cin >> box1Length;
     
-     cout << "Enter Box 2 Height : ";
+    cout << "Enter Box 2 Height : ";
     cin >> box2Height;
     cout << "Enter Box 2 Width : ";
     cin >> box2Width;
     cout << "Enter Box 2 Length : ";
     cin >> box2Length;
-    
-    totalVolume = volume(box1Height, box1Width, box1Length)
-             + volume(box2Height, box2Width, box2Length);
+
+    totalVolume = volume(box1Height, box1Width, box1Length)+ volume(box2Height, box2Width, box2Length);
              
     cout << "Volume of Box is " << totalVolume << endl;
     
